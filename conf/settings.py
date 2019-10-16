84021ee566a72c83060582a84af7968954269ed9
Will Harmon <willcharmon@gmail.com>
2019-10-16 10:30:14 -0400

    initial commit

--- conf/settings.py
+++ conf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o#uuoy%g59gv*inlqyp%#ymd10b-ww2ic+z-vsw)15ro0t0ctd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -76,13 +76,17 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+else:
+    DATABASES = {
+    'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
+    }
 
 
 # Password validation
